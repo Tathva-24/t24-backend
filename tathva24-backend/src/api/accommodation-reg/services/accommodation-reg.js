a3ed5622535e830fdc1b52805668b846f99d6f4d
Rehan Tadpatri <astroanax@outlook.com>
2024-10-17 11:47:15 +0530

    api: create new AccommodationReg collection
    also add new UserReg fields

---
+++ tathva24-backend/src/api/accommodation-reg/services/accommodation-reg.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * accommodation-reg service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::accommodation-reg.accommodation-reg');
